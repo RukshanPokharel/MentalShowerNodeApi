77db6e5c10f6997b3961e8cab61c8bc35e1d0c0f
Botond Horvath <hello@boti7.com>
2022-05-24 18:42:18 +0200

    Use database parameters from environment variables

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "127.0.0.1",
-    USER: "root",
-    PASSWORD: "",
-    Database: "testdb",
+    HOST: process.env.DB_HOST || "127.0.0.1",
+    USER: process.env.DB_USER || "root",
+    PASSWORD: process.env.DB_PASSWORD || "",
+    Database: process.env.DATABASE || "testdb",
     dialect: "mysql",
     port: 3306
     // pool: {
